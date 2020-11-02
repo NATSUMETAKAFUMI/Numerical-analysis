8c0420e74fd14710605f24560a4dad4468548112
Takafumi <s19C1087@s.chibakoudai.jp>
2020-11-02 22:44:48 +0900

    add a file

--- newton.c
+++ newton.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-double f( double x ) {
+double f( double x ) { //f(x)の定義
 
 	return ( 3 * x * x + 2 * x - 5 );
 }
-
-double df( double x ) {
+　
+double df( double x ) { //f'(x)の定義
 
 	return ( 6 * x + 2 );
 }
 
 int main(void) {
 
-	int n = 0;
+	int n = 0; //関数定義
 	int nmax;
 	double x, delta;
 	double eps;
@@ -27,15 +27,15 @@ int main(void) {
 	
 	do {
 		delta = -f(x) / df(x);
-		x = x + delta;
+		x = x + delta
 		n++;
-		printf ("誤差 = %f x = %f\n", delta, x);
+		printf ("誤差 = %f x = %f\n", delta, x); //誤差と計算結果の表示
 	}
 	while ( fabs(delta) > eps && n <= nmax );
 	if ( n == nmax)	{
-		printf ( "解が見つからない\n");
+		printf ( "解が見つからない\n"); //n=nmaxのとき解が見つからない
 	} else {
-		printf ( " x = %fで収束\n" , x);
+		printf ( " x = %fで収束\n" , x);　//n=nmaxでないとき収束
 	}
 	
 	return 0;
